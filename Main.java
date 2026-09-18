7da2295dc53167c22384255291cf4b017974796e
Faith <faithmeryenn@gmail.com>
2026-09-17 17:47:42 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+ public class Main {
+      public static void main(String[] args){
+      Movie m1 = new Movie();
+      Movie m2 = new Movie();
+      Movie m3 = new Movie();     
+      
+      m1.title = " SpiderMan BrandeNewDay ";
+      m1.genre = " (Sci-Fi) ";
+      m1.duration =- 148;
+      m1.displayInfo();
+      
+      m2.title = " Inside Out ";
+      m2.genre = " (Animation) ";
+      m2.duration =- 96;
+      m2.displayInfo();
+      
+      m3.title = " Ponyo ";
+      m3.genre = " (Animation) ";
+      m3.duration =- 101;
+      m3.displayInfo();
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+      }
+    }
---
+++ Movie.java
@@ -0,0 +1,13 @@
+public class Movie {
+    
+    String title;
+    String genre;
+    int duration;
+    
+    public void displayInfo() {
+      System.out.println(title + " " + genre + " " + duration + " minutes ");
+     
+     }
+    }
+      
+      
